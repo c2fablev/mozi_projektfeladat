6e31be67018e939de1400ffe4f0d81ace2e8c232
c2fablev <c2fablev@vasvari.org>
2025-01-27 12:57:01 +0100

    MEGY A HELYKERESÉS!!44!

--- mozi.py
+++ mozi.py
@@ -9,14 +9,35 @@ def feltoltes(lista:list):
         kislista.append("|")
         for j in range(10):
             kislista.append(random.choice(arak))
-            print(len(kislista))
         lista.append(kislista)
     return lista
 
 def jegyVasarlas(szam:int):
-    while szam <=2 or szam >=5:
+    while szam <2 or szam >5:
         print("Hány jegyet szeretne vásárolni? (2-5)", end="")
         szam = int(input())
     return szam
 
+def helykereses(moziTerem:list, helyekSzama:int):
+    egybefuggo = 0
+    i = 1
+    for sor in moziTerem:
+        egybefuggo = 0
+        for hely in sor:
+            if hely == 0:
+                egybefuggo += 1
+            else:
+                egybefuggo = 0
+            if egybefuggo >= helyekSzama:
+                return i
+        i += 1
 
+moziterem = []
+feltoltes(moziterem)
+for sor in moziterem:
+    print(sor)
+jegyek = jegyVasarlas(jegyek)
+if helykereses(moziterem, jegyek) != None:
+    print(helykereses(moziterem, jegyek))
+else:
+    print("nincs hely! :(")
