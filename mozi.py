61682a989382cac5ed1faa3afa7b6d5ed078a33f
c2fablev <c2fablev@vasvari.org>
2025-01-27 12:23:14 +0100

    jegy vásárlás

--- mozi.py
+++ mozi.py
@@ -1,6 +1,6 @@
 import random
 arak = [2500, 2100, 1300, 0]
-
+jegyek = 6
 def feltoltes(lista:list):
     for i in range(15):
         kislista = []
@@ -12,3 +12,11 @@ def feltoltes(lista:list):
             print(len(kislista))
         lista.append(kislista)
     return lista
+
+def jegyVasarlas(szam:int):
+    while szam <=2 or szam >=5:
+        print("Hány jegyet szeretne vásárolni? (2-5)", end="")
+        szam = int(input())
+    return szam
+
+
